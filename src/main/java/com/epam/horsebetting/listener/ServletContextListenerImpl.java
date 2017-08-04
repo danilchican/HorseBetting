c5abbc39208e5cf6e50fa7c2c26d01b4f8b6b4f5
Danilchik Vladislav <danilchican@mail.ru>
2017-08-04 23:23:29 +0300

    Added logging to contextListener.

--- src/main/java/com/epam/horsebetting/listener/ServletContextListenerImpl.java
+++ src/main/java/com/epam/horsebetting/listener/ServletContextListenerImpl.java
@@ -2,18 +2,28 @@ package com.epam.horsebetting.listener;
 
 import com.epam.horsebetting.database.ConnectionPool;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class ServletContextListenerImpl implements ServletContextListener {
 
+    /**
+     * Servlet context.
+     */
+    private ServletContext context;
+
     @Override
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
+    public void contextInitialized(ServletContextEvent ev) {
+        context = ev.getServletContext();
+
         ConnectionPool.getInstance();
+        context.log("Context: Initialized with connection pool.");
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+    public void contextDestroyed(ServletContextEvent ev) {
         ConnectionPool.getInstance().destroy();
+        context.log("Context: Connection pool destroyed.");
     }
 }
